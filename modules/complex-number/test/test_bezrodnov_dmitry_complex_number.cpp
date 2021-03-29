3c3d6fec977e30ba201eca362adf6d54263ecf9f
dmitrybezro <d.bezrodnow@gmail.com>
2021-03-29 16:41:07 +0300

    Correction

--- modules/complex-number/test/test_bezrodnov_dmitry_complex_number.cpp
+++ modules/complex-number/test/test_bezrodnov_dmitry_complex_number.cpp
@@ -9,7 +9,7 @@ TEST(Bezrodnov_Dmitry_ComplexNumberTest, Multiplication_Conjugate_Numbers) {
     double im = 3.0;
     ComplexNumber z1(re, im);
     ComplexNumber z2(re, -im);
-    EXPECT_EQ(10, (z1*z2).getRe());
+    ASSERT_EQ(10, (z1*z2).getRe());
 }
 
 TEST(Bezrodnov_Dmitry_ComplexNumberTest, Sum_Conjugate_Numbers) {
@@ -17,14 +17,14 @@ TEST(Bezrodnov_Dmitry_ComplexNumberTest, Sum_Conjugate_Numbers) {
     double im = 3.0;
     ComplexNumber z1(re, im);
     ComplexNumber z2(re, -im);
-    EXPECT_EQ(2, (z1+z2).getRe());
+    ASSERT_EQ(2, (z1+z2).getRe());
 }
 
 TEST(Bezrodnov_Dmitry_ComplexNumberTest, Module_Conjugate_Numbers) {
     ComplexNumber z1(3, 4);
     ComplexNumber z2(3, -4);
-    EXPECT_EQ(5, static_cast<int>(sqrt(pow(z1.getRe(), 2)
+    ASSERT_EQ(5, static_cast<int>(sqrt(pow(z1.getRe(), 2)
         + pow(z1.getIm(), 2))));
-    EXPECT_EQ(5, static_cast<int>(sqrt(pow(z2.getRe(), 2)
+    ASSERT_EQ(5, static_cast<int>(sqrt(pow(z2.getRe(), 2)
         + pow(z2.getIm(), 2))));
 }
