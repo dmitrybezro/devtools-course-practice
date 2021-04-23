7ae6e8c13f174151d7832767776b06ee80934156
dmitrybezro <d.bezrodnow@gmail.com>
2021-04-24 01:51:47 +0300

    Fix tab

--- modules/Vigener_cipher/test/test_Vigener.cpp
+++ modules/Vigener_cipher/test/test_Vigener.cpp
@@ -5,10 +5,9 @@
 #include"include/Vigener.h"
 
 TEST(VigenerTest, correct_test){
-	Vigener_cipher chip("dmitry");
-	std::string message = "hello world";
-	std::string result_enc = chip.encrypt(message); 
-	std::string result_dec = chip.decrypt(result_enc);
-	ASSERT_EQ(message, result_dec);
-
+    Vigener_cipher chip("dmitry");
+    std::string message = "hello world";
+    std::string result_enc = chip.encrypt(message); 
+    std::string result_dec = chip.decrypt(result_enc);
+    ASSERT_EQ(message, result_dec);
 }
