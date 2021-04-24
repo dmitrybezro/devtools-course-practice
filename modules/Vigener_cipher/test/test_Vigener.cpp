3525102594df2dd38222695abdba6aa65eb9cbcb
dmitrybezro <d.bezrodnow@gmail.com>
2021-04-24 21:43:00 +0300

    Added tests

--- modules/Vigener_cipher/test/test_Vigener.cpp
+++ modules/Vigener_cipher/test/test_Vigener.cpp
@@ -11,3 +11,42 @@ TEST(VigenerTest, correct_test) {
     std::string result_dec = chip.decrypt(result_enc);
     ASSERT_EQ(message, result_dec);
 }
+
+TEST(VigenerTest, empty_test) {
+    Vigener_cipher chip;
+    std::string message = "hello world";
+    std::string result_enc = chip.encrypt(message);
+    std::string result_dec = chip.decrypt(result_enc);
+    ASSERT_EQ(message, result_dec);
+}
+
+TEST(VigenerTest, set_key_test) {
+    Vigener_cipher chip;
+    std::string message = "itmm best institute";
+    chip.set_key("pmi")
+    std::string result_enc = chip.encrypt(message);
+    std::string result_dec = chip.decrypt(result_enc);
+    ASSERT_EQ(message, result_dec);
+}
+
+TEST(VigenerTest, get_key_test) {
+    Vigener_cipher chip;
+    std::string message = "itmm best institute";
+    if (chip.get_key() == "") {
+        chip.set_key("pmi")
+    }
+    std::string result_enc = chip.encrypt(message);
+    std::string result_dec = chip.decrypt(result_enc);
+    ASSERT_EQ(message, result_dec);
+}
+
+TEST(VigenerTest, main_register_test) {
+    Vigener_cipher chip;
+    std::string message = "NNGU best university";
+    if (chip.get_key() == "") {
+        chip.set_key("lobachevsky")
+    }
+    std::string result_enc = chip.encrypt(message);
+    std::string result_dec = chip.decrypt(result_enc);
+    ASSERT_EQ(message, result_dec);
+}
