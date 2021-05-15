d58a2c6ad6661aa3c6c025c77d0a521aa01c2550
dmitrybezro <d.bezrodnow@gmail.com>
2021-05-16 02:08:53 +0300

    Fix position include

--- modules/hanoi_tower/application/application.cpp
+++ modules/hanoi_tower/application/application.cpp
@@ -1,8 +1,8 @@
 // Copyright 2021 Bezrodnov Dmitry
 
-#include "include/hanoi_application.h"
-
 #include <string>
+
+#include "include/hanoi_application.h"
 
 int main(int argc, const char** argv) {
     HanoiApplication app;
